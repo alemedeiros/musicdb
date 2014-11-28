9f3345be30fe24ed85e05c86c344b78e483b88f8
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-11-28 15:07:03 +0000

    Initial commit
    Initial .cabal configuration file and a Hello World Main.hs

---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+-- Main.hs
+--  by Alexandre Medeiros <alexandre.n.medeiros _at_ gmail.com>
+--
+-- Main file for ECS713 Functional Programming Project
+
+
+-- Main function
+main :: IO ()
+main = putStrLn "Hello, World"
